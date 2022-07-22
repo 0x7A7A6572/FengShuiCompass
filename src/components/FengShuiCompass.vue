30885817da07cb332ebf6b8a6d153c01faaa5982
0x7A7A6572 <2623385294@qq.com>
2022-07-23 04:05:15 +0800

    setup

--- src/components/FengShuiCompass.vue
+++ src/components/FengShuiCompass.vue
@@ -1,61 +1,49 @@
 <template>
-  <canvas ref="canvasFengShuiCompass"> 你的浏览器不支持HTML5 Canvas标签 </canvas>
+  <canvas ref="canvasFengShuiCompass">
+    你的浏览器不支持HTML5 Canvas标签
+  </canvas>
 </template>
 
-<script>
-import { FengShuiCompass } from "./compass-main.js";
-import  CompassData  from "./compass-redata.js";
+<script setup>
+import { FengShuiCompass, CompassData } from "./compass-main.js";
+// import  CompassData  from "./compass-redata.js";
 import { ref, onMounted } from "vue";
-export default {
-  name: "FengShuiCompass",
-  props: {
+const props = defineProps({
     width: Number,
     height: Number,
-  },
+  }) 
   // components:{FengShuiCompass},
-  setup(props) {
     let canvasFengShuiCompass = ref(null);
     onMounted(() => {
-      console.log(canvasFengShuiCompass.value,FengShuiCompass);
-      
-// var [_lineWidth, _borderWidth, _fontSpace] = [3, 3, 5];
-// var correctionAngle = -90;
-var canvas = canvasFengShuiCompass.value;
-// var body = document.body;
-// const canvas = res[0].node
-canvas.width = props.width;//body.clientWidth;
-canvas.height = props.height;//body.clientHeight;
-//height * dpr
-var ctx = canvas.getContext('2d');
-// var screenWidth = body.clientWidth ;//screen.availWidth;
-// var screenHeight = body.clientHeight ;//screen.availHeight;
+      console.log(canvasFengShuiCompass.value);
 
-const fs =  new FengShuiCompass();
-// var centralPoint = {
-//     x: screenWidth/2 , y: screenHeight/2 ,
-//   };
+      // var [_lineWidth, _borderWidth, _fontSpace] = [3, 3, 5];
+      // var correctionAngle = -90;
+      var canvas = canvasFengShuiCompass.value;
+      canvas.width = props.width;
+      canvas.height = props.height;
+      var ctx = canvas.getContext("2d");
+      const fs = new FengShuiCompass();
+      let cpData = new CompassData().getAllData();
 
-//  let cpData = new CompassData().getAllData();
-
-
-// fs.setCenterPoint(centralPoint.x,centralPoint.y)
-//   .setRadius(centralPoint.x)
-console.log(props)
-  fs.setCenterPoint(props.width/2,props.height/2)
-  .setRadius(props.height/2)
-  .setLatticeFill([[3,3,'red'],[4,3,'blue']])
-  .setLayerFill([[3,'green']])
-   //.setCompassData(cpData)
-   .setCompassData(CompassData)
-  .draw(ctx)
-
+      // console.log("props", props);
+      fs.setCenterPoint(props.width / 2, props.height / 2)
+        .setRadius(props.height / 2)
+        .setLatticeFill([
+          [3, 3, "red"],
+          [4, 3, "aqua"],
+        ])
+        .setLayerFill([[3, "green"]])
+        .setScaclStyle({
+          minLineHeight: 10,
+          midLineHeight: 25,
+          maxLineHeight: 25,
+          numberFontSize: 30,
+        })
+        .setCompassData(cpData) //必须在配置所有基本数据完成之后执行
+        .draw(ctx); //draw 必须setCompassData完成之后执行 终止链式
     });
-    return {
-      canvasFengShuiCompass,
-      centerXY: [0, 0],
-    };
-  },
-};
+
 </script>
 
 <style scoped>
