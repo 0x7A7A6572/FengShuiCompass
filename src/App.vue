b520d2e56258e2c2c443d3af6c7774d67313260e
0x7A7A6572 <2623385294@qq.com>
2022-07-24 23:22:26 +0800

    改变颜色

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,52 @@
 <script setup>
 import FengShuiCompass from "./components/FengShuiCompass.vue";
+import { CompassData } from "./components/compass-main.js";
+
 import { ref } from "vue";
+let compassDataObj = new CompassData();
 let rotate = ref(0);
+let selectLayer = ref("0");
+let selectLayerColor = ref("#ff0000");
+let selectLattice = ref("");
+let compassData = ref(compassDataObj.getAllData());
+
+let layerFilt = ref([]);
+
+function getDataByIndex(){
+ return compassDataObj.getDataByIndex(selectLayer.value)
+}
+
+function changeLayerFilt(){
+  let count = 0;
+  for(let i in layerFilt.value){
+     console.log("??",layerFilt.value, layerFilt.value[i][0] == selectLayer.value)
+    if(layerFilt.value[i][0] == selectLayer.value){
+      layerFilt.value[i][1].value = selectLayerColor.value;
+      break;
+    }else if(count == layerFilt.value.length){
+      layerFilt.value.push([selectLayer.value, selectLayerColor.value]);
+    }
+    count++;
+  }
+  console.log("LayerFilt.value",layerFilt.value,count);
+}
+
 </script>
 
 <template>
   <div class="gemc-layout">
-    <FengShuiCompass id="gemc" :width="2000" :height="2000" v-model:rotate="rotate"></FengShuiCompass>
+    <FengShuiCompass
+      id="gemc"
+      :width="2000"
+      :height="2000"
+      v-model:rotate="rotate"
+      :compassData="compassData"
+      v-model:layerFilt="layerFilt"
+    ></FengShuiCompass>
     <div class="contorl">
       <div class="control-rotate">
         旋转罗盘
-        <input type="range" min="0" max="100" step="" v-model="rotate"/>
+        <input type="range" min="0" max="100" step="" v-model="rotate" />
       </div>
       <div class="line"></div>
 
@@ -18,15 +54,16 @@ let rotate = ref(0);
         颜色填充
         <div>
           层：
-          <select>
-            <option>1</option>
+          <select v-model="selectLayer">
+            <option v-for="(item,index) in compassDataObj.getAllData()" :key="index">{{index}}</option>
           </select>
-          <input type="color" value="#2D9900" />
+          <input type="color"  @input="changeLayerFilt"/>
+          <!-- <div>{{item}}</div> -->
         </div>
         <div>
           宫：
-          <select>
-            <option>1</option>
+           <select v-model="selectLattice">
+            <option v-for="(item,index) in getDataByIndex()" :key="index">{{item}}</option>
           </select>
           <input type="color" value="#FF9900" />
         </div>
@@ -35,7 +72,7 @@ let rotate = ref(0);
 
       <div class="control-data">
         数据
-        <textarea disabled type="range" min="0" max="100" step="5" ></textarea>
+        <textarea disabled type="range" min="0" max="100" step="5"></textarea>
       </div>
       <div class="line"></div>
     </div>
@@ -73,11 +110,11 @@ body {
 }
 
 .control-data {
-   color: aqua;
+  color: aqua;
   text-align: left;
   font-size: smaller;
 }
-.control-data textarea{
+.control-data textarea {
   font-size: small;
   width: 100%;
 }
